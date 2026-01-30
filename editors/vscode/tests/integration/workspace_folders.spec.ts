d25a21f7bd4f2cf842ac8533ec9c4aff5ca1073d
Sysix <sysix@sysix-coding.de>
2026-01-30 12:42:53 +0100

    test(oxlint/lsp): add init tests on js side

--- editors/vscode/tests/integration/workspace_folders.spec.ts
+++ editors/vscode/tests/integration/workspace_folders.spec.ts
@@ -1,5 +1,5 @@
 import { strictEqual } from "assert";
-import { DiagnosticSeverity, Uri, workspace } from "vscode";
+import { commands, DiagnosticSeverity, Uri, window, workspace } from "vscode";
 import {
   activateExtension,
   getDiagnostics,
@@ -18,9 +18,18 @@ const FIXTURES_URI = Uri.joinPath(WORKSPACE_DIR, "..", "fixtures");
 
 suite("Workspace Folders", () => {
   testMultiFolderMode("shows diagnostics to newly adding folder", async () => {
+    await workspace.getConfiguration("oxc").update("trace.server", "verbose");
+    await workspace.saveAll();
+
     await loadFixture("debugger");
     const folderDiagnostics = await getDiagnostics("debugger.js");
 
+    await commands.executeCommand("oxc.showOutputChannel");
+    await sleep(250);
+    const output = window.activeTextEditor!.document.getText();
+    process.stdout.write(
+      "JS Plugin Output Channel:\n" + output + "\nEnd of JS Plugin Output Channel\n",
+    );
     assert(typeof folderDiagnostics[0].code == "object");
     strictEqual(folderDiagnostics[0].code.target.authority, "oxc.rs");
     strictEqual(folderDiagnostics[0].severity, DiagnosticSeverity.Warning);
